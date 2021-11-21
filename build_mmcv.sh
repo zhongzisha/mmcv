348236a3f308364980b0e4cafa7286d37b95eeae
zhongzisha <zhongzisha@outlook.com>
2021-11-21 22:03:06 +0800

    fix

--- build_mmcv.sh
+++ build_mmcv.sh
@@ -1,1 +1,7 @@
-MMCV_WITH_OPS=1 pip install -e . -vvv
+export MMCV_WITH_OPS=1
+export CUDA_HOME=/share/home/zhongzisha/cuda-10.2-cudnn-8.2.2
+export MAX_JOBS=1
+export TORCH_CUDA_ARCH_LIST="7.0"
+python setup.py build_ext
+python setup.py develop
+#pip install -e . -vvv
